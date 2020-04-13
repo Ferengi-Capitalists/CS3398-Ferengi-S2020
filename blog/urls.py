d3e181d30daadd6a1f0dbb0008ba251d70d66a66
RYAN ALEX FRANKLIN <57727779+Ryan-Franklin@users.noreply.github.com>
2020-04-13 12:10:52 -0500

    Access budget now active with admin Editing

--- blog/urls.py
+++ blog/urls.py
@@ -1,15 +1,10 @@
 from django.urls import path
-from .views import (
-    PostListView, 
-    PostDetailView,
-    PostCreateView,
-    PostUpdateView,
-    PostDeleteView,
-    UserPostListView
-    )
+from .views import *
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
+    path('budget/admin/', admin.site.urls),
     path('', PostListView.as_view(), name='blog-home'),
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
@@ -18,5 +13,11 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
     path('goals/', views.menu, name = 'goals'),
-    path('news/', views.news, name = 'news')
+    path('news/', views.news, name = 'news'),
+
+    path('budget/', BudgetView.as_view(), name='budget_view'),
+    path('bills/', BillListView.as_view(), name='bills_view'),
+    path('payroll/', PayrollListView.as_view(), name='payroll_view'),
+    path('expenses/', ExpensesListView.as_view(), name='expenses_view'),
+    path('reports/', report_view, name='reports_view')
 ]
